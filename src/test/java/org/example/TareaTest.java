c997064463608a5832eff265280517a7dedaf004
Joaco <joaquinnogallar@gmail.com>
2024-10-29 21:02:58 -0300

    feat: setUp y tearDown del test de tarea

--- src/test/java/org/example/TareaTest.java
+++ src/test/java/org/example/TareaTest.java
@@ -1,5 +1,21 @@
 package org.example;
 
+import org.example.entity.Tarea;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class TareaTest {
+    private Tarea tarea;
+
+    @BeforeEach
+    public void setUp() {
+        tarea =  new Tarea(1L, "Lavar platos", "Lavar toda la vajilla antes de ir a dormir");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        tarea = null;
+    }
+
 
 }
